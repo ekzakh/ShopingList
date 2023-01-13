0fc035155cd445095a7971c46e17661b736e17c9
EkZakh <ekzakh14@gmail.com>
2023-01-13 05:56:09 +0300

    prepared shopItem layout

---
+++ app/src/main/java/com/terrinc/shopinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.terrinc.shopinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.terrinc.shopinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
